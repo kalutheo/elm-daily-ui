0542dbee49f8cdbb960851143f6672053f4facd1
Theophile Kalumbu <theophile@ouicar.fr>
2016-12-09 18:20:44 +0100

    04-weather add HTTP call weather api

--- 04-weather-widget/src/Model.elm
+++ 04-weather-widget/src/Model.elm
@@ -4,3 +4,25 @@ module Model exposing (..)
 type alias Model =
     { message : String
     }
+
+
+type alias Coord =
+    { lon : Float
+    , lat : Float
+    }
+
+
+type alias Weather =
+    { temp : Float
+    , pressure : Int
+    , humidity : Int
+    , temp_min : Float
+    , temp_max : Float
+    }
+
+
+type alias ResponseApiWeather =
+    { coord : Coord
+    , weather : Weather
+    , name : String
+    }
