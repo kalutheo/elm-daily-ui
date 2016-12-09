cf146872eaa31a9cf98b8900b26870d5d700b92a
Theophile Kalumbu <theophile@ouicar.fr>
2016-12-09 14:27:57 +0100

    04-weather add Weather geolocation

--- 04-weather-widget/src/App.elm
+++ 04-weather-widget/src/App.elm
@@ -3,20 +3,44 @@ module App exposing (..)
 import Html exposing (Html, text, div)
 import View.Widget exposing (..)
 import Model exposing (Model)
+import Geolocation
+import Task
 
 
 init : ( Model, Cmd Msg )
 init =
-    ( { message = "Your Elm App is working!" }, Cmd.none )
+    ( { message = "Your Elm App is working!" }, geolocate )
+
+
+geolocate : Cmd Msg
+geolocate =
+    Task.attempt GeocodeResult Geolocation.now
 
 
 type Msg
     = NoOp
+    | GeocodeResult (Result Geolocation.Error Geolocation.Location)
 
 
 update : Msg -> Model -> ( Model, Cmd Msg )
 update msg model =
-    ( model, Cmd.none )
+    case msg of
+        NoOp ->
+            ( model, Cmd.none )
+
+        GeocodeResult (Ok response) ->
+            let
+                _ =
+                    Debug.log "GeocodeResult:OK" response
+            in
+                ( model, Cmd.none )
+
+        GeocodeResult (Err error) ->
+            let
+                _ =
+                    Debug.log "GeocodeResult:ERR" error
+            in
+                ( model, Cmd.none )
 
 
 view : Model -> Html Msg
