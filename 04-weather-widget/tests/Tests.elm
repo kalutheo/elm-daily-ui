014edb43f5aa0e007f975950354e29211def407a
Theophile Kalumbu <theo@art-ev.com>
2016-12-11 22:10:08 +0100

    04-weather all tests covered

--- 04-weather-widget/tests/Tests.elm
+++ 04-weather-widget/tests/Tests.elm
@@ -1,27 +1,10 @@
 module Tests exposing (..)
 
 import Test exposing (..)
-import Expect
-import String
-import App
 import TestsWeather
-
-
-defaultTests : List Test
-defaultTests =
-    [ test "App.model.message should be set properly" <|
-        \() ->
-            Expect.equal (Tuple.first App.init |> .message) "What's the weather today ?"
-    , test "Addition" <|
-        \() ->
-            Expect.equal (3 + 7) 10
-    , test "String.left" <|
-        \() ->
-            Expect.equal "a" (String.left 1 "abcdefg")
-    ]
 
 
 all : Test
 all =
     describe "A Test Suite"
-        (List.concat [ TestsWeather.tests, defaultTests ])
+        (List.concat [ TestsWeather.tests ])
